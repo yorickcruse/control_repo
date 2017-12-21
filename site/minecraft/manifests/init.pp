6a1a28e55687e3103ffc8fbd06ddc4ded731216b
Yorick Cruse <yorick.cruse@lightspeedretail.com>
2017-12-21 16:02:05 -0500

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,6 +1,7 @@
-class minecraft {
-  $url = 'https://s3.amazonaws.com/Minecraft.Download/versions/1.12.1/minecraft_server.1.12.1.jar'
+class minecraft (
+  $url = 'https://s3.amazonaws.com/Minecraft.Download/versions/1.12.1/minecraft_server.1.12.1.jar',
   $install_dir = '/opt/minecraft'
+  ){
   file {$install_dir:
     ensure => directory,
   }
