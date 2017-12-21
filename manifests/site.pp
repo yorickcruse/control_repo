a073541b967d5a461c2830c12a91a2d14b1dd6dd
Yorick Cruse <yorick.cruse@lightspeedretail.com>
2017-12-21 13:18:57 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,12 +1,17 @@
 node default {
   file { '/root/README':
     ensure => file,
-    content => 'This is a Yooo',
+    content => 'This is a readme',
     owner   => 'root',
   }
 }
 node 'master.puppet.vm' {
   include role::master_server
+  file {'/root/README':
+    ensure => file,
+    content => “Welcome to ${fqdn}”,
+    owner => 'root',
+  }
 }
 node /^web/ { 
   include role::app_server
