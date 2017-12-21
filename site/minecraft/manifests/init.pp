6ac4823e2bae932b0edd8a3eaf18ae09dca79606
Yorick Cruse <yorick.cruse@lightspeedretail.com>
2017-12-21 16:07:37 -0500

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -19,8 +19,9 @@ class minecraft (
   }
   file {'/etc/systemd/system/minecraft.service':
     ensure => file,
-    source => 'puppet:///modules/minecraft/minecraft.service',
-  }
+    content => epp('minecraft/minecraft.service.epp', {
+      install_dir => $install_dir,
+    }),
   service { 'minecraft':
     ensure => running,
     enable => true,
